f0e941ff8249dd02ed02d33de5b351c38e814cbe
Sonsehwan45 <sahwan458@gmail.com>
2025-10-10 01:26:21 +0900

    Feat[BE](evaluation): evaluation 생성

--- backend/src/main/java/com/back/domain/evaluation/controller/EvaluationController.java
+++ backend/src/main/java/com/back/domain/evaluation/controller/EvaluationController.java
@@ -1,28 +1,34 @@
 package com.back.domain.evaluation.controller;
 
 
+import com.back.domain.evaluation.dto.EvaluationCreateReq;
 import com.back.domain.evaluation.service.EvaluationService;
+import com.back.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/evaluations")
 public class EvaluationController {
 
     private final EvaluationService evaluationService;
 
     //평가 생성
-    @PostMapping("/evaluation")
-    public ResponseEntity<Void> createEvaluation(@RequestBody EvalutaionCreateReq request){
-        //임의로 생성자의 아이디 생성
-        Long currentUserId = 1L;
+    @PostMapping
+    public ApiResponse<Void> createEvaluation(@RequestBody EvaluationCreateReq request) {
+        // TODO: Spring Security에서 현재 로그인한 사용자의 ID를 가져와야 합니다.
+        // Principal principal
+        // Long currentUserId = ((Member) principal).getId(); 와 같은 방식
+        Long currentUserId = 1L; // 임시 테스트용 ID
 
         evaluationService.createEvaluation(currentUserId, request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return new ApiResponse<>("201", "평가가 성공적으로 등록되었습니다.", null);
     }
 }
--- backend/src/main/java/com/back/domain/evaluation/service/EvaluationService.java
+++ backend/src/main/java/com/back/domain/evaluation/service/EvaluationService.java
@@ -1,8 +1,13 @@
 package com.back.domain.evaluation.service;
 
+import com.back.domain.evaluation.dto.EvaluationCreateReq;
 import com.back.domain.evaluation.repository.ClientEvaluationRepository;
 import com.back.domain.evaluation.repository.FreelancerEvaluationRepository;
+import com.back.domain.member.member.constant.Role;
+import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.repository.MemberRepository;
+import com.back.domain.project.project.entity.Project;
+import com.back.domain.project.project.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +19,20 @@ public class EvaluationService {
     private final FreelancerEvaluationRepository freelancerEvaluationRepository;
     private final ClientEvaluationRepository clientEvaluationRepository;
     private final MemberRepository memberRepository;
+    private final ProjectRepository projectRepository;
 
+    @Transactional
+    public void createEvaluation(Long evaluatorId, EvaluationCreateReq evaluationCreateReq){
+        Member evaluatorMember = memberRepository.findById(evaluatorId)
+                .orElseThrow(()-> new IllegalArgumentException("평가자를 찾을 수 없습니다."));
 
+        Project project = projectRepository.findById(evaluationCreateReq.projectId())
+                .orElseThrow(() -> new IllegalArgumentException("프로젝트를 찾을 수 없습니다."));
+
+        //평가 평균 계산
+        EvaluationCreateReq.Ratings ratings = evaluationCreateReq.ratings();
+        double average = (ratings.professionalism() + ratings.scheduleAdherence() + ratings.communication() + ratings.proactiveness())/4.0;
+
+        int satisfactionScore = (int) Math.round(average);
+    }
 }
