8c2597571437a9e72550ca66f2a938352c13f030
inti0 <sonin223@gmail.com>
2025-10-14 17:15:02 +0900

    🧪 Test : 권한 검증 테스트 추가

--- backend/src/test/java/com/back/domain/freelancer/freelancer/controller/ApiV1FreelancerControllerTest.java
+++ backend/src/test/java/com/back/domain/freelancer/freelancer/controller/ApiV1FreelancerControllerTest.java
@@ -10,6 +10,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.back.domain.freelancer.freelancer.dto.FreelancerSearchCondition;
 import com.back.domain.freelancer.freelancer.dto.FreelancerSummary;
 import com.back.domain.freelancer.freelancer.service.FreelancerService;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -35,6 +39,8 @@ class ApiV1FreelancerControllerTest {
     private MockMvc mvc;
     @Autowired
     private FreelancerService freelancerService;
+    @Autowired
+    private MemberService memberService;
 
     @Test
     @DisplayName("목록보기 응답 - 200")
@@ -64,7 +70,8 @@ class ApiV1FreelancerControllerTest {
                 .andDo(print());
 
         // TODO: 데이터 확인
-        Page<FreelancerSummary> freelancers = freelancerService.findAll(new FreelancerSearchCondition(null, null, null), PageRequest.of(0, 20));
+        Page<FreelancerSummary> freelancers = freelancerService.findAll(new FreelancerSearchCondition(null, null, null),
+                PageRequest.of(0, 20));
 
         resultActions
                 .andExpect(jsonPath("$.resultCode").value("200"))
@@ -111,9 +118,12 @@ class ApiV1FreelancerControllerTest {
 
     @Test
     @DisplayName("프리랜서 정보 업데이트 - 성공")
+    @WithUserDetails(value = "freelancer1", userDetailsServiceBeanName = "customUserDetailsService")
     void t2() throws Exception {
-        Page<FreelancerSummary> freelancers = freelancerService.findAll(new FreelancerSearchCondition(null, null, null), PageRequest.of(0, 20));
-        FreelancerSummary freelancerSummary = freelancers.stream().findFirst().orElseThrow(() -> new RuntimeException("프리랜서 없음"));
+        Page<FreelancerSummary> freelancers = freelancerService.findAll(new FreelancerSearchCondition(null, null, null),
+                PageRequest.of(0, 20));
+        FreelancerSummary freelancerSummary = freelancers.stream().findFirst()
+                .orElseThrow(() -> new RuntimeException("프리랜서 없음"));
         Long id = freelancerSummary.id();
 
         ResultActions resultActions = mvc
@@ -146,5 +156,40 @@ class ApiV1FreelancerControllerTest {
                 .andExpect(jsonPath("$.data.id").value(id))
                 .andExpect(jsonPath("$.data.job").value("백엔드"))
                 .andExpect(jsonPath("$.data.freelancerEmail").value("myEmail@email.com"));
+    }
+
+    @Test
+    @DisplayName("프리랜서 정보 업데이트(403) - 권한 없음")
+    @WithUserDetails(value = "freelancer1", userDetailsServiceBeanName = "customUserDetailsService")
+    void t2_forbidden() throws Exception {
+        Member member = memberService.findByUsername("freelancer1")
+                .orElseThrow(() -> new RuntimeException("freelancer1 없음"));
+
+        Long id = member.getId();
+        Assertions.assertThat(freelancerService.findById(id + 1)).isNotNull();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        put("/api/v1/freelancers/{id}", id + 1)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                                "job" : "백엔드",
+                                                "freelancerEmail" : "myEmail@email.com",
+                                                "comment" : "안녕하세요",
+                                                "career" : {"Java": 5, "Spring": 4, "AWS": 3},
+                                                "skillIds" : [1, 2]
+                                        }
+                                        """)
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1FreelancerController.class))
+                .andExpect(handler().methodName("updateFreelancer"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.resultCode").value(403))
+                .andExpect(jsonPath("$.msg").value("본인의 정보만 수정할 수 있습니다."))
+                .andExpect(jsonPath("$.data").doesNotExist());
     }
 }
